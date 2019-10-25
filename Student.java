2785638734e3d2f868919ea528cbefdaff588c43
zuka beridze <zaqaria.beridze@sangu.edu.ge>
2019-10-25 09:49:09 +0400

    first file

---
+++ Student.java
@@ -0,0 +1,33 @@
+public class Student {
+    private String name, surname;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "name='" + name + '\'' + '}';
+    }
+}
